cb9c415f159dc92a13205a86791478d372d22fe6
MartinWachira <67173409+Ares-ui@users.noreply.github.com>
2022-10-16 21:44:48 +0300

    Update GreedyBFS.py

--- GreedyBFS.py
+++ GreedyBFS.py
@@ -1,35 +1,57 @@
-import networkx as nx
-import matplotlib.pyplot as plt
+graph = {'SportsComplex': [('Siwaka', 450)],
+         'Siwaka': [('Phase1Ent.A', 10), ('Phase1Ent.B', 230)],
+         'Phase2Ent.A': [('Mada', 850), ('Phase2Ent.B', 100)],
+         'Phase1Ent.B': [('Phase2', 112), ('STC', 50)],
+         'STC': [('Phase2', 50), ('ParkingLot', 250)],
+         'Phase2': [('J1', 600), ('Phase3', 500), ('STC', 50)],
+         'J1': [('Mada', 200)],
+         'Phase3': [('ParkingLot', 350)],
+         'Mada': [('ParkingLot', 700)],
+         }
 
-g = nx.Graph()
-nodes = ["StrathmoreSportsComplex", "Siwaka", "Phase1Ent.A", "Phase1Ent.B", "Phase2", "J1", "Mada", "STC", "Phase3",
-         "Parking Lot"]
+H_table = {
+    'SportsComplex': 730,
+    'Siwaka': 405,
+    'Phase1Ent.A': 380,
+    'Phase1Ent.B': 280,
+    'STC': 213,
+    'Phase2': 210,
+    'J1': 500,
+    'Phase3': 160,
+    'Mada': 630,
+    'ParkingLot': 0
+}
 
-g.add_node("StrathmoreSportsComplex")
-g.add_node("Siwaka")
-g.add_node("Phase1Ent.A")
-g.add_node("Phase1Ent.B")
-g.add_node("Phase2")
-g.add_node("J1")
-g.add_node("Mada")
-g.add_node("STC")
-g.add_node("Phase3")
-g.add_node("Parking Lot")
 
-g.add_edge("StrathmoreSportsComplex", "Siwaka", weight="450")
-g.add_edge("Siwaka", "Phase1Ent.A", weight="10")
-g.add_edge("Siwaka", "Phase1Ent.B", weight="230")
-g.add_edge("Phase1Ent.A", "Phase1Ent.B", weight="100")
-g.add_edge("Phase1Ent.B", "STC", weight="50")
-g.add_edge("Phase1Ent.A", "Mada", weight="850")
-g.add_edge("Phase1Ent.B", "Phase2", weight="112")
-g.add_edge("STC", "Phase2", weight="50")
-g.add_edge("STC", "Parking Lot", weight="250")
-g.add_edge("Phase2", "Phase3", weight="500")
-g.add_edge("Phase2", "J1", weight="600")
-g.add_edge("J1", "Mada", weight="200")
-g.add_edge("Phase3", "Parking Lot", weight="350")
-g.add_edge("Mada", "Parking Lot", weight="700")
+def path_h_cost(path):
+    g_cost = 0
+    for (node, cost) in path:
+        g_cost += cost
+    last_node = path[-1][0]
+    h_cost = H_table[last_node]
+    # f_cost = g_cost + h_cost
+    return h_cost, last_node
 
-nx.draw(g, node_size=8000, with_labels=True, )
-plt.savefig("filename.png")
+
+def Greedy_best_first_search(graph, start, goal):
+    visited = []
+    queue = [[(start, 0)]]
+    while queue:
+        queue.sort(key=path_h_cost)
+        path = queue.pop(0)
+        node = path[-1][0]
+        if node in visited:
+            continue
+        visited.append(node)
+        if node == goal:
+            return path
+        else:
+            adjacent_nodes = graph.get(node, [])
+            for (node2, cost) in adjacent_nodes:
+                new_path = path.copy()
+                new_path.append((node2, cost))
+                queue.append(new_path)
+
+
+solution = Greedy_best_first_search(graph, 'SportsComplex', 'ParkingLot')
+print('Solution is', solution)
